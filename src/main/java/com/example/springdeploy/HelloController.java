e51261c53cbc2ff022f5e5e6055c206dd15d150c
salvascript <salvascript@gmail.com>
2022-10-31 18:40:12 +0100

    spring heroku app config

--- src/main/java/com/example/springdeploy/HelloController.java
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${app.varexample}")
+    @Value("${app.message}")
     String message;
 
     @GetMapping("/hola")
